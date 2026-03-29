664cbcf662a069511bc8e4d53dacf81aaec02c48
Dziica <peca04nish@gmail.com>
2026-03-29 21:55:54 +0200

    Merge cart feature into main

--- main.py
+++ main.py
@@ -4,22 +4,17 @@ from cart import Cart
 
 manager = ProductManager()
 
-p1 = Product("Laptop", 1000, 2)
-p2 = Product("Mouse", 20, 10)
-p3 = Product("Keyboard", 50, 5)
+p1 = Product("Phone", 800, 1)
+p2 = Product("Headphones", 100, 3)
+p3 = Product("Monitor", 300, 2)
 
 manager.add_product(p1)
 manager.add_product(p2)
 manager.add_product(p3)
 
 cart = Cart()
-
-# dodavanje 3 proizvoda u korpu
 cart.add_to_cart(p1)
 cart.add_to_cart(p2)
 cart.add_to_cart(p3)
 
-print("\nCART CONTENT:")
-cart.display_cart()
-
-print("TOTAL CART VALUE:", cart.total_price())
+print("CART TOTAL:", cart.total_price())
